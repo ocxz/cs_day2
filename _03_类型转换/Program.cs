7000ed7795a8dc9d2a620e4ff9226e79ec5b3c43
ocxz <2252779530@qq.com>
2019-07-09 02:22:32 +0800

    第二次提交，增加了Covert类型转换和自增自减的学习

---
+++ _03_类型转换/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _03_类型转换
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 非兼容类型转换学习
+
+            // 强制类型转换    自动类型转换   条件:类型兼容
+            // int --> double  double --> int
+            // 类型不兼容转换
+            //Console.WriteLine(Convert.ToDouble("123.12"));
+            //Console.WriteLine(Convert.ToInt32("14"));
+            //Console.ReadKey();
+
+            #endregion
+
+            #region 用户输入姓名 语文 数学 英语 计算总成绩和平均成绩
+
+            //Console.WriteLine("请输入你的姓名");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("请输入你的语文成绩");
+            //string strChinese = Console.ReadLine();
+            //Console.WriteLine("请输入你的数学成绩");
+            //string strMath = Console.ReadLine();
+            //Console.WriteLine("请输入你的英语成绩");
+            //string strEnglish = Console.ReadLine();
+
+            //// 字符串相加,最终会相连接  若需参与计算则需转换为int或double
+            ////int chinese = Convert.ToInt32(strChinese);
+            ////int math = Convert.ToInt32(strMath);
+            ////int english = Convert.ToInt32(strEnglish);
+            ////Console.WriteLine("{0}同学,你的总成绩是{1}分,平均成绩是{2:0.0}分",name,chinese+math+english, (chinese+math+english)/3.0);
+            ////Console.ReadKey();
+
+            //double chinese = Convert.ToDouble(strChinese);
+            //double math = Convert.ToDouble(strMath);
+            //double english = Convert.ToDouble(strEnglish);
+            //Console.WriteLine("{0}同学,你的总成绩是{1}分,平均成绩是{2:0.0}分", name, chinese + math + english, (chinese + math + english) / 3);
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Convert类型转换2  计算用户输入数字的两倍
+
+            Console.WriteLine("请输入一个数字");
+            Console.WriteLine(Convert.ToDouble(Console.ReadLine()) * 2);
+            Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
---
+++ _04_加加减减/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace _04_加加减减
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 自增自减的学习使用
+            //int number = 10;
+            //// number++  先使用后加一
+            //// ++number  先加一后使用
+            //// 等同于:number = number + 1;
+            //Console.WriteLine(number++);   // 10
+            //Console.WriteLine(++number);   // 12
+
+            //// number--  先使用后减一
+            //// --number  先减一后使用
+            //// 等同于:number = number + 1;
+            //Console.WriteLine(number--);
+            //Console.WriteLine(--number);
+            //Console.ReadKey();
+            #endregion
+
+            #region 判别符号放前与放后的区别
+
+            // 放前:先加减,加减完后再使用
+            // 放后:先使用,使用完后再加减
+            // 最终的值都是要加减一的
+
+            //int number = 10;
+            //int number2 = 10;
+            //int result = 10 + number++;    // 先使用后加一   20
+            //int result2 = 10 + ++number2;   // 先加一后使用  21
+            //Console.WriteLine(number);    // 11
+            //Console.WriteLine(result);    // 20
+            //Console.WriteLine(number2);    // 11
+            //Console.WriteLine(result2);    // 21
+            //Console.ReadKey();
+
+            //// result = 10 + ++number等效于:
+            //int number = 10;
+            //number++;
+            //int result = 10 + number;
+            //Console.WriteLine(number);   // 11
+            //Console.WriteLine(result);  // 21
+            //Console.ReadKey();
+
+            //// result = 10 + number++等效于:
+            //int number = 10;
+            //int result = 10 + number;
+            //number++;
+            //Console.WriteLine(number);   // 11
+            //Console.WriteLine(result);  // 20
+            //Console.ReadKey();
+
+            //// result = 10 + --number等效于:
+            //int number = 10;
+            //number--;
+            //int result = 10 + number;
+            //Console.WriteLine(number);   // 9
+            //Console.WriteLine(result);  // 19
+            //Console.ReadKey(); 
+
+            //// result = 10 + number--等效于:
+            //int number = 10;
+            //int result = 10 + number;
+            //number--;
+            //Console.WriteLine(number);   // 9
+            //Console.WriteLine(result);  // 20
+            //Console.ReadKey();
+
+            //int a = 5;
+            //int b = a++ + ++a * 2 + --a + a++;  // 相当于:5+7*2+6+6=>5+14+12=31
+            //Console.WriteLine(a);   // 7
+            //Console.WriteLine(b);   // 31
+            //Console.ReadKey();
+
+
+            #endregion
+        }
+    }
+}
