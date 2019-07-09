c23a862bf6597398c4ee33e627857cc7b1e3b0d8
ocxz <2252779530@qq.com>
2019-07-09 15:45:30 +0800

    第二天学习结束

--- _09_练习/Program.cs
+++ _09_练习/Program.cs
@@ -111,6 +111,85 @@ namespace _09_练习
             //}
 
             #endregion
+
+            #region 提示用户输入账号密码，若都正确，则登录成功，若账号错误提示账号错误，密码错误提示密码错误
+
+            //Console.WriteLine("请输入用户名");
+            //string username = Console.ReadLine();
+            //if (username == "admin")
+            //{
+            //    Console.WriteLine("请输入用户密码");
+            //    string password = Console.ReadLine();
+            //    if (password == "password")
+            //    {
+            //        Console.WriteLine("恭喜您，登录成功");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("密码输入错误，请点击任意键重新输入");
+            //        Console.WriteLine("请重新输入密码");
+            //        password = Console.ReadLine();
+            //        if (password == "password")
+            //        {
+            //            Console.WriteLine("恭喜您，登录成功，您一共输入密码2次");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("登录失败，二次密码输入错误，程序退出");
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("用户名不存在，请按任意键重新输入");
+            //    Console.WriteLine("请重新输入用户名");
+            //    username = Console.ReadLine();
+            //    Console.WriteLine("请输入用户密码");
+            //    string password = Console.ReadLine();
+            //    if (username == "admin" && password == "password")
+            //    {
+            //        Console.WriteLine("登录成功，共输入两次");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("登录失败，两次输入错误，程序退出");
+            //    }
+            //}
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region 输入年龄，>=18可查看，<10不可查看，>=10且<18提示，选择yes继续查看，选择no退出，放弃查看
+
+            //Console.WriteLine("请输入您的年龄");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //if (age >= 18)
+            //{
+            //    Console.WriteLine("您已成年，请放心查看");
+            //}else if (age >= 10)
+            //{
+            //    Console.WriteLine("此内容不适宜青少年长期浏览，请确是否继续查看");
+            //    Console.ReadKey();
+            //    Console.WriteLine("输入yes继续查看，否则退出查看");
+            //    string keyInput = Console.ReadLine();
+            //    if (keyInput == "yes" || keyInput == "Yes")
+            //    {
+            //        Console.WriteLine("继续查看，请注意身体");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("退出，放弃查看");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("未成年人不得浏览此等信息，退出");
+            //}
+
+            //Console.ReadKey();
+            #endregion
+
         }
     }
 }
